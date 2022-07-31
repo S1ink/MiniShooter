c3bb793ddf70674645f1856fab067399cadbe2f0
Sam Richter <hoodie888@outlook.com>
2022-07-31 10:54:58 -0500

    setup processing frame

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,18 @@
 #include <array>
+#include <vector>
+
+#include <CameraServer.h>
 
 #include <core/visionserver2.h>
 #include <core/extensions.h>
+#include <core/vision.h>
 
 #include "input.h"
 #include "pwm_l298n.h"
 #include "stepper.h"
 
 
-class SquareTarget : public vs2::VPipeline<SquareTarget> {
+class TPipeline : public vs2::VPipeline<TPipeline> {
 public:
 	inline static const std::array<cv::Point3f, 4> world{
 		cv::Point3f(0.25f, 0.25f, 0.f), 	//top-right
@@ -17,11 +21,27 @@ public:
 		cv::Point3f(-0.25f, 0.25f, 0.f),	//top-left
 	};
 
-	SquareTarget() : VPipeline<SquareTarget>("Retroreflective square tracking") {}
+	TPipeline() : VPipeline<TPipeline>("Retroreflective square tracking") {
+		// ntables
+	}
 
+	inline void connect(const VisionCamera& v) { this->setCamera(v); }
+	void invoke(cv::Mat& io_frame) {
+		this->getFrame(io_frame);
+		this->process(io_frame);
+		this->PutFrame(io_frame);
+	}
 	void process(cv::Mat& io_frame) override {
 
 	}
+
+	inline float getX() const { return this->tvec[0][0]; }
+	inline float getY() const { return this->tvec[1][0]; }
+	inline float getZ() const { return this->tvec[2][0]; }
+	double getDist() const { return sqrt(pow(getX(), 2) + pow(getY(), 2) + pow(getZ(), 2)); }
+	double getLR() const { return atan2(getX(), getZ()) * 180/M_PI; }
+	double getUD() const { return atan2(getY(), getZ()) * -180/M_PI; } 
+	const cv::Mat_<float>& getTranslation() const { return this->tvec; }
 
 private:
 	std::array<cv::Mat, 3> channels;
@@ -46,9 +66,33 @@ private:
 
 
 int main(int argc, char** argv) {
+
+	gpioInitialise();
+	
 	W24BYJ_Stepper<6, 13, 19, 26> hood_motor{};
 	PWM_L298N<21, 20, 16> shooter_motor{};
 	PWM_L298N<25, 7, 8> feed_motor{};
+
+	std::vector<VisionCamera> cameras;
+	TPipeline vpipe;
+	cv::Mat frame;
+	cs::MjpegServer stream{frc::CameraServer::GetInstance()->AddServer("Stream")};
+
+	readConfig(cameras);
+	for(size_t i = 0; i < cameras.size(); i++) {
+		if(cameras[i].IsConnected()) {
+			std::iter_swap(cameras.begin(), cameras.begin() + i);
+			break;
+		}
+	}
+	vpipe.connect(cameras[0]);
+	stream.SetSource(vpipe);
+
+	for(;;) {
+		vpipe.invoke(frame);	// grab frame, process, and output to stream
+
+		// update motors accordingly
+	}
 
 
 }
